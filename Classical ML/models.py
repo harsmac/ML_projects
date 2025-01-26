1b8ffeaa5322c851258b73bf30945cce6d6d018d
harsmac <harshitha.acad@gmail.com>
2025-01-26 16:45:49 +0530

    more code :)

--- Classical ML/models.py
+++ Classical ML/models.py
@@ -36,23 +36,67 @@ class LinearRegression:
         y_test = x_test @ self.wts
         return y_test
 
+# add GD version of LR
+
 
 class LogisticRegression:
-    def __init__(self):
-        pass
+    def __init__(self, n_iters=10, lr=0.01):
+        self.wts = None
+        self.bias = None
+        self.lr = lr
+        self.n_iters = n_iters
+    
+    def sigmoid(self, X, wts, b):
+        num = X @ wts + b * np.ones((X.shape[0], 1))
+        return 1/(1 + np.exp(-num))
     
     def fit(self, X, y):
-        pass
+        N, d = X.shape
+        self.wts = np.random.rand(d, 1)
+        self.bias = 0
+        for _ in range(self.n_iters):
+            sigm_x = self.sigmoid(X, self.wts, self.bias)
+            # neg_log_likelihood_loss = - np.mean(y @ np.log(sigm_x) + (1 - y) @ np.log(1 - sigm_x))
+            
+            # derivatives
+            db = (1/N) * (sigm_x - y.reshape(N, 1))
+            dw = X.T @ (db)
+            
+            self.wts -= self.lr * dw
+            self.bias -= self.lr * db
+        
+        return self.wts, self.bias
     
     def predict(self, x_test):
-        pass
+        return self.sigmoid(x_test, self.wts, self.bias)
 
 class SVM:
-    def __init__(self):
-        pass
+    def __init__(self, n_iters=10, lamb=0.01, lr=0.01):
+        self.wts = None
+        self.bias = None
+        self.n_iters = n_iters
+        self.lamb = lamb
+        self.lr = lr
     
     def fit(self, X, y):
-        pass
+        N, d = X.shape
+        self.wts = np.random.rand((d, 1))
+        self.bias = 0
+        
+        for _ in range(self.n_iters):
+            curr_prod = y @ (X @ self.wts + self.bias * np.ones(N, 1)) - 1
+            
+            dw = np.zeros_like(curr_prod)  # Initialize dw with zeros
+            dw[curr_prod > 0] = X[curr_prod > 0].T @ y[curr_prod > 0]  # Set dw where curr_prod > 0
+            dw = dw + self.lamb * self.wts
+            db = np.zeros_like(curr_prod)
+            db[curr_prod > 0] = y[curr_prod > 0]
+            
+            self.wts -= self.lr * dw
+            self.bias -= self.lr * db
+        
+        return self.wts, self.bias
+            
     
     def predict(self, x_test):
         pass
